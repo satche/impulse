12b5f06be7b571ea01397d08111b4c6cdc21f1e4
Satche <info@satche.ch>
2023-12-22 11:42:08 +0100

    Add left turn road part

--- Assets/Scripts/RoadGenerator.cs
+++ Assets/Scripts/RoadGenerator.cs
@@ -15,28 +15,38 @@ public class RoadGenerator : MonoBehaviour
 
     void Start()
     {
+        GenerateRoad();
+    }
+
+    /// <summary>
+    /// Generates a road by randomly selecting road parts from a list
+    /// Instantiate them in a sequence, connecting the end of each part to the start of the next part.
+    /// </summary>
+    private void GenerateRoad()
+    {
         // Generate the road
         for (int i = 0; i < roadLength; i++)
         {
             // Take a random road part from the list
-            int random = Random.Range(0, roadParts.Count);
-            GameObject roadPart = roadParts[random];
+            int roadPartIndex = Random.Range(0, roadParts.Count);
+            GameObject roadPart = roadParts[roadPartIndex];
 
             // Instantiate the road part, put it in current GameObject origin
             int roadPartInstanceCount = gameObject.transform.childCount;
-            GameObject roadPartInstance = Instantiate(roadPart, gameObject.transform.position, Quaternion.identity);
-            roadPartInstance.transform.SetParent(gameObject.transform);
+            GameObject nextRoadPart = Instantiate(roadPart, gameObject.transform.position, Quaternion.identity);
+            nextRoadPart.transform.SetParent(gameObject.transform);
 
             // If it's not the first road part, put it at the end of the previous one
             if (roadPartInstanceCount > 0)
             {
                 GameObject previousRoadPart = gameObject.transform.GetChild(roadPartInstanceCount - 1).gameObject;
                 GameObject previousRoadPartEnd = previousRoadPart.transform.Find("End").gameObject;
-                GameObject roadPartInstanceStart = roadPartInstance.transform.Find("Start").gameObject;
-                
-                roadPartInstance.transform.rotation = previousRoadPartEnd.transform.rotation;
-                Vector3 offset = roadPartInstanceStart.transform.position - roadPartInstance.transform.position;
-                roadPartInstance.transform.position = previousRoadPartEnd.transform.position - offset;
+                GameObject nextRoadPartStart = nextRoadPart.transform.Find("Start").gameObject;
+
+                // Align the start of the next road part to the end of the previous one
+                nextRoadPart.transform.rotation = Quaternion.LookRotation(previousRoadPartEnd.transform.forward, previousRoadPartEnd.transform.up);
+                Vector3 offset = nextRoadPartStart.transform.position - nextRoadPart.transform.position;
+                nextRoadPart.transform.position = previousRoadPartEnd.transform.position - offset;
             }
         }
     }
